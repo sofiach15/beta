cf1c3ab003182bd641b257ba0ad2ec30b4ed2301
Sofia <sofiachirvechesdelgado@gmail.com>
2025-06-06 21:32:04 -0500

    Script: data types in Python

---
+++ example1.py
@@ -0,0 +1,24 @@
+#This is an Example with Python
+
+'''
+Date: 06-jun_2025
+Dev: Sofia Chirveches
+'''
+
+data1 = 10
+data2 = 3,50
+data3 = "Sofia"
+data4 = 'Chirveches'
+data5 = '''
+Hello people, we are learning Python.
+'''
+data6 = True
+data7 = 3.4
+
+print(type(data1))
+print(type(data2))
+print(type(data3))
+print(type(data4))
+print(type(data5))
+print(type(data6))
+print(type(data7))
